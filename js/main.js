53215b6fd9062f7a4332180e0b1c9903a40a2d26
Vipul Tiwari <viper.vt21@gmail.com>
2022-01-23 15:40:08 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,52 +1,23 @@
-$(window).scroll(function() {    
-    var scroll = $(window).scrollTop();
-
-    if (scroll >= 50) {
-        $(".main-header").addClass("sticky");
-    } else {
-        $(".main-header").removeClass("sticky");
-    }
-});
-
 $(document).ready(function () {
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 50) {
+            $(".main-header").addClass("sticky");
+        } else {
+            $(".main-header").removeClass("sticky");
+        }
+    });
 
     if (window.matchMedia("(max-width: 767px)").matches) {
-        
-        $(".hamburger").click(function(){
+
+        $(".hamburger").click(function () {
             $(".hamburger").toggleClass("active");
-            $("navigation").toggleClass("active");
+            $(".navigation").toggleClass("active");
             $(".nav-items").removeClass("active");
         });
-    
-        $('.m_nav').click(function(){
-            $(this).toggleClass("rotateArrow")
-            $(this).next().toggleClass("active");
-        });
-
-           $(".search_container").toggle();
-        });
-        
-        $('.footer_links h3').click(function(){
-            $(this).next().slideToggle();
-            $(this).toggleClass("rotateArrow");
-        }); $('.search_icon').click(function(){
-		
-
     }
 
-    $('.search_icon a').click(function(){
-        $(".search_container").toggle();
-    });
-
-    $('#heroSlider').owlCarousel({
-        loop: false,
-        nav: false,
-        dots: true,
-        margin: 0,
-        navText: ['', ''],
-        autoplay: false,
-        items: 1
-    });
 
     $('#ruPaySlider').owlCarousel({
         loop: false,
@@ -70,137 +41,13 @@ $(document).ready(function () {
         }
     });
 
-    $('#ruPayCardSlider').owlCarousel({
-        loop: false,
-        nav: true,
-        dots: false,
-        margin: 10,
-        navText: ['', ''],
-        autoplay: false,
-        responsive: {
-            0: {
-                items: 1,
-                nav: false,
-                dots: true
-            },
-            600: {
-                items: 1
-            },
-            1000: {
-                items: 1
-            }
-        }
-    });
-
-    $('#articleSlider').owlCarousel({
-        loop: false,
-        nav: false,
-        dots: false,
-        margin: 0,
-        navText: ['', ''],
-        autoplay: false,
-        responsive: {
-            0: {
-                items: 1,
-                dots: true
-            },
-            600: {
-                items: 2
-            },
-            1000: {
-                items: 3
-            }
-        }
-    });
-
-    $('.logo_slider').owlCarousel({
-        loop: true,
-        nav: false,
-        dots: true,
-        margin: 20,
-        navText: ['', ''],
-        autoplay: true,
-        responsive: {
-            0: {
-                items: 2,
-                dots: true
-            },
-            600: {
-                items: 3
-            },
-            1000: {
-                items: 5
-            }
-        }
-    });
-
-    $('#categoryOffers').owlCarousel({
-        loop: false,
-        nav: true,
-        dots: false,
-        margin: 20,
-        navText: ['', ''],
-        autoplay: false,
-        responsive: {
-            0: {
-                items: 3,
-                nav: false,
-                dots: true
-            },
-            600: {
-                items: 3,
-                nav: false,
-                dots: true
-            },
-            1000: {
-                items: 6
-            }
-        }
-    });
-
-    //Input Fields
-    (function () {
-        $('.input_container')
-            .find('.floatlabel')
-            .each(function () {
-                $(this).on('blur', function () {
-                    $this = $(this);
-                    if (this.value !== "") {
-                        $this.addClass('filled');
-                    }
-                    else {
-                        $this.removeClass('filled');
-                    }
-                });
-            });
-    })();
-
     // smooth scrolling navigation
-    $(".inner_banner_content a").click( function() {
+    $(".inner_banner_content a").click(function () {
         var target = $(this).attr("href");
         $("body, html").animate({
             scrollTop: $(target).offset().top - 80
         }, 300);
         return false;
-    });
-
-    //Filter
-    $('.filters ul li').click(function(){
-        $('.filters ul li').removeClass('active');
-        $(this).addClass('active');
-
-        var data = $(this).attr('data-filter');
-        $grid.isotope({
-            filter: data
-        })
-    });
-
-    var $grid = $(".grid").isotope({
-        itemSelector: ".all",
-        percentPosition: true,
-        masonry: {
-            columnWidth: ".all"
-        }
     });
 
 });
